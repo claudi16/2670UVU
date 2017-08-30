2ab972ba101cee58bb990d2f8030f61589aeb3fa
claudi <claudi1618@gmail.com>
2017-08-30 14:05:43 -0600

    Added Flip&Animate

--- Scripting2670/Assets/Scripts/MoveCharacter.cs
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -6,15 +6,19 @@ public class MoveCharacter : MonoBehaviour {
 
 	CharacterController cc;
 
+	Vector3 tempMove;
+
+	public float speed = 5;
+
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent<CharacterController>();
-		MoveInput.KeyAction = Move;
+		MoveInput.KeyAction += Move;
 	}
-	
 
 	// Update is called once per frame
 	void Move (float _movement) {
-		print(_movement);
+		tempMove.x = _movement*speed*Time.deltaTime;
+		cc.Move(tempMove);
 	}
 }
