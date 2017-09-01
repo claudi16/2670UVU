25e8a3023ca1580864af8d5c721416ca02cc7f17
claudi <claudi1618@gmail.com>
2017-09-01 16:13:39 -0600

    Jump..ish

--- Scripting2670/Assets/Scripts/MoveCharacter.cs
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -9,23 +9,33 @@ public class MoveCharacter : MonoBehaviour {
 	Vector3 tempMove;
 
 	public float speed = 5;
+	public float gravity = 1;
+    public float jumpHeight = 0.2f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		cc = GetComponent<CharacterController>();
 
 			PlayButton.Play += OnPlay;
 	}
-
 	void OnPlay() {
+		MoveInput.JumpAction = Jump;
 		MoveInput.KeyAction += Move;
 		PlayButton.Play -= OnPlay;
 	}
-	
+	void Jump (){
+		print("Jump");
+		tempMove.y += jumpHeight;
+		//if jump count =2 reset jump count.
+		//if grounded = true ******Create int, add number to int everytime space is hit, fire off function until certain number is reached.
+	}
 
 	// Update is called once per frame
 	void Move (float _movement) {
+		tempMove.y -= gravity*Time.deltaTime;
 		tempMove.x = _movement*speed*Time.deltaTime;
 		cc.Move(tempMove);
 	}
 }
+
+//USE A COUNT FOR A DOUBLE JUMP
--- Scripting2670/Assets/Scripts/MoveInput.cs
+++ Scripting2670/Assets/Scripts/MoveInput.cs
@@ -6,9 +6,14 @@ using System;
 public class MoveInput : MonoBehaviour {
 
     public static Action<float> KeyAction;
+    public static Action JumpAction;
 
 	// Update is called once per frame
 	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpAction();
+        }
         if  (KeyAction !=null)
         {
             KeyAction(Input.GetAxis("Horizontal"));
