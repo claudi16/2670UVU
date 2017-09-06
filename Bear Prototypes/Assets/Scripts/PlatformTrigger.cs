99186ee44a74b3a00b4ec7400b6b50d960f7df72
claudi <claudi1618@gmail.com>
2017-09-06 00:07:41 -0600

    SetActive Platform Triggers

---
+++ Bear Prototypes/Assets/Scripts/PlatformTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour {
+	private GameObject pp;
+
+    void Start(){
+		pp = GameObject.Find("PlatformParent");
+		pp.gameObject.SetActive(false);
+
+    }
+
+ 	void OnTriggerEnter(Collider other){
+		if(other.name == "Boy"){
+			print("poo");
+        foreach(Transform child in pp.transform){
+			pp.gameObject.SetActive(true);
+			
+		}
+	 }	 
+    }
+
+}
